eb46129f1cf314d50167d5633a5b3886d2efd408
pdeguzman <padgzmn@gmail.com>
2022-07-24 16:06:34 -0400

    added displayed value on the size slider

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const sort = new Sort(50, 10, "bubbleSort");
 const compareSingleAlgoBtn = document.getElementById('compare-single-algo-btn');
 const playPauseBtn = document.getElementById('play-pause-btn');
 const resetBtn = document.getElementById('reset-btn');
+//DEA
+const sizeSlider = document.getElementById('size');
+const speedSlider = document.getElementById('speed');
 
 compareSingleAlgoBtn.addEventListener('click', function() {
   console.log('Switch to compare screen');
@@ -15,3 +18,12 @@ playPauseBtn.addEventListener('click', function() {
 resetBtn.addEventListener('click', function() {
   sort.reset();
 })
+
+//DEA
+let i = document.querySelector('input'),
+    o = document.querySelector('output');
+o.innerHTML = i.value;
+i.addEventListener('input', function() {
+    o.innerHTML = i.value;
+}, false);
+
